4687d00022dcb57279df4b4a4cd9009d4c880faa
horoto9089 <horoto1005@gmail.com>
2024-11-02 01:55:10 +0900

    desk

--- app1.py
+++ app1.py
@@ -3,50 +3,80 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly
+import skmob
+import os
+from PIL import Image
+import folium
+from streamlit_folium import st_folium
 
 st.set_page_config(layout="wide")
 
-st.snow()
+df = pd.read_csv('./data/tdf_data.csv')
+tdf = skmob.TrajDataFrame(df, latitude='lat', longitude='lng',
+                             datetime='time', user_id='user_id')
+
+image_path = "./picture/yamap.jfif"
+img1 = Image.open(image_path)
+
+# HTMLファイルの内容を読み込み
+with open("./picture/trajectory_map.html", "r") as f:
+    html_data = f.read()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Sidebar menu with links to each section by using HTML anchor tags
 st.sidebar.markdown('''
 # MENU
-- [purpose and significance](#purpose-and-significance)
-- [model](#model)
-- [data](#data)
-- [result](#result)
-- [conclusion](#conclusion)
+- [生データ](#row_data)
+- [データの整形・加工](#data_processing)
+- [ハイキング関数](#hiking_function)
+- [時系列歩行速度データ](#pace)
 ''', unsafe_allow_html=True)
 
 
-st.title("Repository of Graduation Thesis")
+st.title("富士登山者のGPSデータ分析")
+st.image(img1)
 
-
-
-
-st.header("研究目的・意義", anchor="purpose-and-significance")
+st.header("生データ", anchor="row_data")
 st.markdown("---")
-st.text("自動販売機は公共財（災害時物資提供媒体、地域情報、AEDなど）の供給面でも地域への大切な役割がある。")
+st.text("yamapアプリから富士山に登る登山者のGPSデータを2024年8月10,11日の96の活動データをスクレイピング")
 
-st.text("しかしながら、自動販売機台数は減少傾向にある。")
+st.text("")
+st.write(df)
 
-st.text("よって、自動販売機普及への取り組み・分布傾向の分析が必要である。")
+st.components.v1.html(
+    html_data,
+    height=600,  # 初期の高さを設定、画面サイズに合わせて調整される
+    width=None,  # 幅を指定しないことで画面幅に応じて調整される
+    scrolling=True
+)
 
-with st.expander("English.ver"):
-    st.write("Vendingmachines have an important role to play in the community in terms of supplying public goods (medium for providing supplies during disasters, local information, AED, etc.). However, the number of vendingmachines is on the decline. Therefore, efforts to promote the use of vending machines are needed.")
+with st.expander("pythonコード"):
+    st.write("")
 
 
 
 
 
 
-
-
-st.header("モデル", anchor="model")
+st.header("データの整形・加工", anchor="data_processing")
 st.markdown("---")
 
 
-with st.expander("English.ver"):
+with st.expander("pythonコード"):
     st.write("このようにして、ユーザーがクリックすると、追加の情報を表示できるようになります。")
 
 
@@ -58,12 +88,12 @@ with st.expander("English.ver"):
 
 
 
-st.header("データ", anchor='data')
+st.header("ハイキング関数", anchor='hiking_function')
 st.markdown("---")
 
 
 
-with st.expander("English.ver"):
+with st.expander("pythonコード"):
     st.write("このようにして、ユーザーがクリックすると、追加の情報を表示できるようになります。")
 
 
@@ -75,13 +105,13 @@ with st.expander("English.ver"):
 
 
 
-st.header("結果・考察", anchor='result')
+st.header("時系列歩行速度データ", anchor='pace')
 st.markdown("---")
 
 
 
 
-with st.expander("English.ver"):
+with st.expander("pythonコード"):
     st.write("このようにして、ユーザーがクリックすると、追加の情報を表示できるようになります。")
 
 
@@ -91,13 +121,4 @@ with st.expander("English.ver"):
 
 
 
-
-st.header("結論", anchor='conclusion')
-st.markdown("---")
-
-
-
-
-with st.expander("English.ver"):
-    st.write("このようにして、ユーザーがクリックすると、追加の情報を表示できるようになります。")
 
